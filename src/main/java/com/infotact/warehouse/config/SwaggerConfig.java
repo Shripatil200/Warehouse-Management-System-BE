1051ce9b42206ddcb3c457b0f79d2015ab190346
Shripatil200 <patilshriram200@gmail.com>
2026-05-15 22:27:45 +0530

    fix(doc): swagger doc error fix by changing dependency and confix logic

--- src/main/java/com/infotact/warehouse/config/SwaggerConfig.java
+++ src/main/java/com/infotact/warehouse/config/SwaggerConfig.java
@@ -1,15 +1,38 @@
 package com.infotact.warehouse.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springdoc.core.utils.SpringDocUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    static {
-        // This solves the 500 error when returning Spring Data Page objects
-        SpringDocUtils.getConfig().replaceWithClass(
-                org.springframework.data.domain.Pageable.class,
-                org.springdoc.core.converters.models.PageableAsQueryParam.class
-        );
+//
+//    static {
+//        SpringDocUtils.getConfig().replaceWithClass(
+//                org.springframework.data.domain.Pageable.class,
+//                org.springdoc.core.converters.models.PageableAsQueryParam.class
+//        );
+//    }
+
+    @Bean
+    public OpenAPI warehouseOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Warehouse Management System API")
+                        .description("API documentation for Warehouse Management System")
+                        .version("v1.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
